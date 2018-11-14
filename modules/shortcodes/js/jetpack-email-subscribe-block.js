5d888e0dda36b058899beb37fca8ed758f0d198f
artpi <artur.piszek@gmail.com>
2018-11-14 18:58:36 +0100

    Better GB configuration

--- modules/shortcodes/js/jetpack-email-subscribe-block.js
+++ modules/shortcodes/js/jetpack-email-subscribe-block.js
@@ -6,48 +6,40 @@ const el = wp.element.createElement,
 	TextControl = wp.components.TextControl,
 	InspectorControls = wp.editor.InspectorControls;
 
+const fields = [
+	{ id: 'title', label: 'Title' },
+	{ id: 'email_placeholder', label: 'Placeholder' },
+	{ id: 'submit_label', label: 'Submit button label' },
+	{ id: 'consent_text', label: 'Consent text' },
+	{ id: 'processing_label', label: '"Processing" status message' },
+	{ id: 'success_label', label: 'Success status message' },
+	{ id: 'error_label', label: 'Error status message' },
+];
+
 registerBlockType( 'jetpack/email-subscribe', {
 	title: 'Email Subscribe Block',
 	icon: 'email',
 	category: 'widgets',
 
 	edit: function( props ) {
+		const edit_controls = fields.map( function( field ) {
+			return el( InspectorControls, {},
+				el( TextControl, {
+					label: field.label,
+					value: props.attributes[ field.id ],
+					onChange: function( value ) {
+						props.setAttributes( { [ field.id ]: value } );
+					},
+				} )
+			);
+		} );
+
 		return [
-			/*
-			 * The ServerSideRender element uses the REST API to automatically call
-			 * php_block_render() in your PHP code whenever it needs to get an updated
-			 * view of the block.
-			 */
 			el( ServerSideRender, {
 				block: 'jetpack/email-subscribe',
 				attributes: props.attributes,
 			} ),
-			/*
-			 * InspectorControls lets you add controls to the Block sidebar. In this case,
-			 * we're adding a TextControl, which lets us edit the 'foo' attribute (which
-			 * we defined in the PHP). The onChange property is a little bit of magic to tell
-			 * the block editor to update the value of our 'foo' property, and to re-render
-			 * the block.
-			 */
-			el( InspectorControls, {},
-				el( TextControl, {
-					label: 'Title',
-					value: props.attributes.title,
-					onChange: function( value ) {
-						props.setAttributes( { foo: value } );
-					},
-				} )
-			),
-			el( InspectorControls, {},
-				el( TextControl, {
-					label: 'Email Placeholder',
-					value: props.attributes.email_placeholder,
-					onChange: function( value ) {
-						props.setAttributes( { foo: value } );
-					},
-				} )
-			),
-		];
+		].concat( edit_controls );
 	},
 
 	// We're going to be rendering in PHP, so save() can just return null.
